ec68d5b32ed0fc92774e26a701dcee1342139232
Alexander Pikeev <alexanderpikeev@gmail.com>
2023-08-11 13:53:01 +0300

    chore(graphql)!: drop graphql support
    BREAKING CHANGE: graphql queries are no longer supported for api

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -1,11 +1,9 @@
 package v1
 
 import (
-	graphqlhandler "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/vgekko/ani-go/graph"
 	"github.com/vgekko/ani-go/internal/controller/http/v1/middleware"
 	"github.com/vgekko/ani-go/internal/usecase"
 	"golang.org/x/exp/slog"
@@ -26,21 +24,11 @@ func NewRouter(handler *gin.Engine, uc *usecase.UseCase, log *slog.Logger) {
 
 	v1 := handler.Group("/v1")
 	{
-		v1.POST("/graphql", graphqlHandler(uc, log))
-
 		search := v1.Group("/search")
 		{
 			newInfoRoutes(search, uc.InfoUseCase, log)
 			newLinkRoutes(search, uc.LinkUseCase, log)
 		}
 
-	}
-}
-
-func graphqlHandler(uc *usecase.UseCase, log *slog.Logger) gin.HandlerFunc {
-	h := graphqlhandler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{Uc: uc, Log: log}}))
-
-	return func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
